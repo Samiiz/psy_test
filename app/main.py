610ef6fdf7f33649b50646107c414a3f67b26843
Samiiz <skboss159@gmail.com>
2024-03-22 05:57:21 +0900

    create temporary path

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run("main:app", reload=True)
