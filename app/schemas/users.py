6f21ac7376ea2e6887656a47f8caf3fdf690737e
Samiiz <skboss159@gmail.com>
2024-03-22 06:37:40 +0900

    create users schema

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,2 +1,13 @@
 from pydantic import BaseModel
 from typing import List, Optional
+
+class UserBase(BaseModel):
+    name : str
+
+class User(UserBase):
+    id : int
+    age : int
+    gender : str
+
+    class Config:
+        orm_mode = True
