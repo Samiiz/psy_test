8c67cfa992009eb10997f48e61daddbee76c7b6d
Samiiz <skboss159@gmail.com>
2024-03-22 23:16:22 +0900

    create CRUD for users

--- app/crud/sql.py
+++ app/crud/sql.py
@@ -1,24 +1,111 @@
 from fastapi import HTTPException
-from schemas.users import CreateUser, User
+from sqlalchemy import select, insert, update, delete
 from sqlalchemy.orm import Session
-from sqlalchemy.sql import text
+from sqlalchemy.exc import NoResultFound
+from models import User as UserModel, Answer as AnswerModel
+from schemas.users import CreateUser, User, UpdateUser
+from schemas.answers import Answer
+# from schemas.users import CreateUser, User
+# from sqlalchemy.sql import text
 
-def create_user(db: Session, user: CreateUser):
-    user_data = user.dict()
+# sqlalchemy core의 기능을 이용한 가동성 좋은 동기식 CRUD
 
-    sql = text("INSERT INTO users (name, age, gender) VALUES (:name, :age, :gender) RETURNING id")
-    result = db.execute(sql, params=user_data)
+def create_user(db: Session, user: CreateUser) -> User:
+    sql = insert(UserModel).values(name=user.name, age=user.age, gender=user.gender).returning(UserModel.id)
+    result = db.execute(sql)
     db.commit()
-
-    new_user_id = result.fetchone()[0]
+    new_user_id = result.fetchone().id
 
     if new_user_id is None:
-        raise HTTPException(status_code=400, detail="Could not find user_id")
+        raise HTTPException(status_code=400, detail="User Not Created")
+    try:
+        new_user = db.query(UserModel).filter(UserModel.id == new_user_id).one()
+    except NoResultFound:
+        raise HTTPException(status_code=404, detail="User Not Found")
 
-    user_query = text("SELECT * FROM users WHERE id = :new_user_id")
-    user_result = db.execute(user_query, {"new_user_id": new_user_id}).fetchone()
+    return User.from_orm(new_user)
 
-    if user_result is None:
-        raise HTTPException(status_code=404, detail="Could not find user")
+def get_user_id(db: Session, user_id: int) -> User:
+    sql = select(UserModel).where(UserModel.id == user_id)
+    try:
+        result = db.execute(sql).scalar_one()
+    except NoResultFound:
+        raise HTTPException(status_code=404, detail="User not found")
 
-    return User.from_orm(user_result)
+    return User.from_orm(result)
+
+def get_users(db: Session, skip: int = 0, limit: int = 10) -> list[User]:
+    sql = select(UserModel).offset(skip).limit(limit)
+    results = db.execute(sql).scalars().all()
+
+    users = []
+    for user in results:
+        user_data = User.from_orm(user)
+        answer_sql = select(AnswerModel).where(AnswerModel.user_id == user.id)
+        answer_results = db.execute(answer_sql).scalars().all()
+        user_data.answers = [Answer.from_orm(answer) for answer in answer_results]
+        users.append(user_data)
+
+    return users
+
+def update_user(db: Session,  user_id: int, update_user: UpdateUser) -> User:
+    user_data = update_user.dict()
+    sql = update(UserModel).where(UserModel.id == user_id).values(**user_data)
+    db.execute(sql)
+    db.commit()
+
+    user = get_user_id(db, user_id)
+    
+    return user
+
+def delete_user(db: Session,  user_id: int) -> User:
+    user = get_user_id(db, user_id)
+
+    sql = (
+        delete(UserModel).
+        where(UserModel.id == user_id)
+    )
+    db.execute(sql)
+    db.commit()
+
+    return user
+
+
+# 수동쿼리 버전
+# def create_user(db: Session, user: CreateUser):
+#     user_data = user.dict()
+
+#     sql = text("INSERT INTO users (name, age, gender) VALUES (:name, :age, :gender) RETURNING id")
+#     result = db.execute(sql, params=user_data)
+#     db.commit()
+
+#     new_user_id = result.fetchone()[0]
+
+#     if new_user_id is None:
+#         raise HTTPException(status_code=400, detail="Could not find user_id")
+
+#     user_query = text("SELECT * FROM users WHERE id = :new_user_id")
+#     user_result = db.execute(user_query, {"new_user_id": new_user_id}).fetchone()
+
+#     if user_result is None:
+#         raise HTTPException(status_code=404, detail="Could not find user")
+
+#     return User.from_orm(user_result)
+
+# def get_user_id(db: Session, user_id: int):
+#     sql = text("SELECT * FROM users WHERE id = :user_id")
+#     result = db.execute(sql, {"user_id": user_id}).fetchone()
+#     return User.from_orm(result)
+
+# def get_users(db: Session, skip: int = 0, limit: int = 10):
+#     sql = text("SELECT * FROM users LIMIT :limit OFFSET :skip")
+#     result = db.execute(sql, {"limit": limit, "skip": skip}).fetchall()
+#     users = [User.from_orm(user) for user in result]
+
+#     for user in users:
+#         answer_sql = text("SELECT * FROM answers WHERE user_id = :user_id")
+#         answer_results = db.execute(answer_sql, {"user_id": user['id']}).fetchall()
+#         user['answers'] = [answer._asdict() for answer in answer_results]
+
+#     return users
+
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,11 +1,34 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from typing import Union
+from typing import Union, List
 import crud.sql as sql, schemas.users as users, database
 
 router = APIRouter()
 
 @router.post('/', response_model=users.User)
 def create_user(user: users.CreateUser, db: Session = Depends(database.get_db)):
-    db_user = sql.create_user(db, user)
+    return sql.create_user(db, user)
+
+@router.get('/{user_id}', response_model=users.User)
+def get_user(user_id: int, db: Session = Depends(database.get_db)):
+    user_id = int(user_id)
+
+    if isinstance(user_id, int):
+        return sql.get_user_id(db, user_id)
+
+@router.get('/', response_model=List[users.User])
+def get_users(skip: int, limit: int=10, db: Session = Depends(database.get_db)):
+    return sql.get_users(db, skip, limit)
+
+@router.put('/{user_id}', response_model=users.User)
+def update_user(user_id: int, user: users.UpdateUser, db: Session = Depends(database.get_db)):
+    return sql.update_user(db, user_id, user)
+
+@router.delete('/{user_id}', response_model=users.User)
+def delete_user(user_id: int, db: Session = Depends(database.get_db)):
+    db_user = sql.delete_user(db, user_id)
+
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User Not Found")
+    
     return db_user
